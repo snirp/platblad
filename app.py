942ee5878cc0bd2eaea8fb8a36d48c33d86c3a67
Roy Prins <prinsroy@gmail.com>
2014-02-25 18:31:28 +0100

    translations (to be removed)

--- app.py
+++ app.py
@@ -8,31 +8,31 @@ from flask import Flask, Markup, url_for, render_template, abort, render_templat
 
 app = Flask(__name__)
 
+
 #platblad
 class Pages(object):
     default_language = 'en'
     _cache = {}  # shared cache
 
-    def __init__(self, folder=u'pages', suffix='.md', ):
+    def __init__(self, folder=u'pages', suffix='.md'):
         self.folder = folder
         self.suffix = suffix
 
     def root(self):
         return os.path.join(app.root_path, self.folder)
 
-    def all(self, lang=''):
-        pagedir = os.path.join(self.root(), lang)
+    def all(self, lang=default_language):
+        pagedir = os.path.join(self.root(), '' if lang == self.default_language else lang)
         if not os.path.isdir(pagedir):
             abort(404)
         for filename in os.listdir(pagedir):
             if filename.endswith(self.suffix):
                 yield self.get(filename[:-len(self.suffix)], lang)
 
-    def get(self, name, lang=''):
+    def get(self, name, lang=default_language):
         filepath = os.path.join(self.root(), '' if lang == self.default_language else lang, name+self.suffix)
         if not os.path.isfile(filepath):
             abort(404)
-        lang = lang or self.default_language
         mtime = os.path.getmtime(filepath)
         page, old_mtime = self._cache.get(filepath, (None, None))
         if not page or mtime != old_mtime:
@@ -52,20 +52,16 @@ class Page(object):
         self.body = body
         self.lang = lang
 
-    @werkzeug.cached_property
-    def meta(self):
-        return yaml.safe_load(self.head) or {}
-
     def __getitem__(self, name):
         return self.meta[name]
 
     @werkzeug.cached_property
+    def meta(self):
+        return yaml.safe_load(self.head) or {}
+
+    @werkzeug.cached_property
     def html(self):
         return markdown(render_template_string(Markup(self.body)), ['codehilite', 'fenced_code'])
-
-    def url(self, **kwargs):
-        return url_for(
-            'page_detail', name=self.name, **kwargs)
 
     def updated(self):
         return self.meta.get('lastmod', self['published'])
@@ -79,18 +75,15 @@ def by_date(pages):
 
 
 #controllers
-@app.route('/test/')
-def test():
-    return "hallo wereld"
-
 @app.route('/blog/')
-@app.route('/blog/<lang>/')
-def blogs_list(lang=''):
+@app.route('/<lang>/blog/')
+def blogs_list(lang=blog.default_language):
     blogs = [b for b in by_date(blog.all(lang))]
     return render_template('blog-list.html', **locals())
 
-@app.route('/blog/<lang>/<name>')
-def blog_detail(name, lang):
+@app.route('/blog/<name>')
+@app.route('/<lang>/blog/<name>')
+def blog_detail(name, lang=blog.default_language):
     b = blog.get(name, lang)
     return render_template('blog-detail.html', **locals())
 
